5a85c02e8f860e2ef3033d4051d8a4a4274b7ec5
Joao Pedro <joaopedro99_stylle@hotmail.com>
2023-09-03 16:08:16 -0300

    front + back

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -1,6 +1,18 @@
-import '@/styles/globals.css'
+import { ChakraProvider } from '@chakra-ui/react';
 import type { AppProps } from 'next/app';
+import { Inter } from 'next/font/google';
+import '@/styles/globals.css';
+import theme from '../theme';
+
+const inter = Inter({ subsets: ['latin'] });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  // TODO: api loading thing
+  return (
+    <ChakraProvider theme={theme} cssVarsRoot="body">
+      <main className={inter.className}>
+        <Component {...pageProps} />
+      </main>
+    </ChakraProvider>
+  );
 }
